356d5865e37ee2068f46494943ad772bdda24455
R.G. Wood <ric@grislyeye.com>
2025-11-08 12:06:05 +0000

    Add: show version number

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const fs = require("fs");
 const { marked } = require("marked");
+const package = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
 const markdown = fs.readFileSync("README.md", "utf8");
 const template = fs.readFileSync("index.html", "utf8");
 const html = marked.parse(markdown);
+const compiled = template
+  .replace(/\_\_MARKDOWN\_\_/g, html)
+  .replace(/\_\_VERSION\_\_/g, package.version);
 
-console.log(template.replace(/\_\_MARKDOWN\_\_/g, html));
+console.log(compiled);
